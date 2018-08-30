b66b9dce609289f22825c7d4ac2f38a3739619b9
Dave Rogers <thedude@yukondude.com>
2018-08-30 12:06:55 -0700

    Extract publication date and slug metadata from filename.

--- pelicanconf.py
+++ pelicanconf.py
@@ -24,6 +24,8 @@ DEFAULT_LANG = 'en'
 
 TYPOGRIFY = True
 
+FILENAME_METADATA = '(?P<date>\d{4}-\d{2}-\d{2})-(?P<slug>.*)'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
